58deb0f13f4148db233f640cf3fe5f8fd6d94dea
Sazedur <sazedur11ab@gmail.com>
2025-07-30 00:21:53 +0600

    books

--- src/components/ListedBooks/ListedBooks.jsx
+++ src/components/ListedBooks/ListedBooks.jsx
@@ -3,7 +3,7 @@
 const ListedBooks = () => {
     return (
         <div>
-            <h2>Listed books</h2>
+            <h2 className="text-2xl font-bold text-green-600 text-center">Listed books</h2>
         </div>
     );
 };
